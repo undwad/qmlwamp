e2c411ffe542a8d0cd194a835f5fb18849e795f2
Сельченков Николай <undwad@integra-s.com>
2015-03-20 15:23:01 +0400

    +

--- qmlwebsockets/websocketclient.h
+++ qmlwebsockets/websocketclient.h
@@ -54,12 +54,14 @@ signals:
     void socketError(const QString& message, const QString& details);
 
 public slots:
-    void open(const QString& url, const QString& origin, const QString& extensions, const QString& protocol, bool mask)
+    void open(const QString& url, const QString& origin, const QString& extensions, const QString& protocol, bool mask, bool ignoreSslErrors)
     {
         close();
 
         _mask = mask;
+        _ignoreSslErrors = ignoreSslErrors;
         _input_header = QString();
+        _input_data.clear();
 
         QUrl url_(url);
         if("ws" == url_.scheme()) _ssl = false;
@@ -115,6 +117,7 @@ public:
         connect(&_socket, &QTcpSocket::aboutToClose, this, &WebSocketWorker::aboutToClose);
         connect(&_socket, &QTcpSocket::disconnected, this, &WebSocketWorker::disconnected);
 #       if !defined(QT_NO_SSL)
+        connect(&_sslsocket, &QSslSocket::connected, this, &WebSocketWorker::handshake);
         connect(&_sslsocket, &QSslSocket::encrypted, this, &WebSocketWorker::connected);
         connect(&_sslsocket, &QSslSocket::readyRead, this, &WebSocketWorker::readyRead);
         connect(&_sslsocket,SIGNAL(error(QAbstractSocket::SocketError)), this, SLOT(error(QAbstractSocket::SocketError)));
@@ -135,6 +138,10 @@ public:
     }
 
 private slots:
+#   if !defined(QT_NO_SSL)
+    void handshake() { if(_ignoreSslErrors) _sslsocket.ignoreSslErrors(); }
+#   endif
+
     void connected()
     {
         emit stateChanged(_state = ReadyState::INITIALIZING);
@@ -162,7 +169,9 @@ private slots:
         }
         case ReadyState::OPEN:
         {
-            qDebug() << socket().readAll();
+            QByteArray packet = socket().readAll();
+            _input_data.insert(_input_data.end(), packet.begin(), packet.end());
+            parseInputData();
             break;
         }
         }
@@ -188,10 +197,12 @@ private:
     QSslSocket _sslsocket;
 #endif
     bool _ssl = false;
+    bool _ignoreSslErrors = false;
     QString _output_header;
     QString _input_header;
     ReadyState _state = ReadyState::CLOSED;
     bool _mask;
+    std::vector<qint8> _input_data;
 
     inline QAbstractSocket& socket()
     {
@@ -263,6 +274,91 @@ private:
 
         socket().write((char*)buffer.data(), buffer.size());
     }
+
+    void parseInputData()
+    {
+        while (true)
+        {
+            wsheader_type ws;
+            if (_input_data.size() < 2) return; // Need at least 2
+            const quint8 * data = (quint8*) &_input_data[0]; // peek, but don't consume
+            ws.fin = (data[0] & 0x80) == 0x80;
+            ws.opcode = (wsheader_type::opcode_type) (data[0] & 0x0f);
+            ws.mask = (data[1] & 0x80) == 0x80;
+            ws.N0 = (data[1] & 0x7f);
+            ws.header_size = 2 + (ws.N0 == 126? 2 : 0) + (ws.N0 == 127? 8 : 0) + (ws.mask? 4 : 0);
+            if (_input_data.size() < ws.header_size) return; // Need: ws.header_size - _input_data.size()
+            int i;
+            if (ws.N0 < 126)
+            {
+                ws.N = ws.N0;
+                i = 2;
+            }
+            else if (ws.N0 == 126)
+            {
+                ws.N = 0;
+                ws.N |= ((quint64) data[2]) << 8;
+                ws.N |= ((quint64) data[3]) << 0;
+                i = 4;
+            }
+            else if (ws.N0 == 127)
+            {
+                ws.N = 0;
+                ws.N |= ((quint64) data[2]) << 56;
+                ws.N |= ((quint64) data[3]) << 48;
+                ws.N |= ((quint64) data[4]) << 40;
+                ws.N |= ((quint64) data[5]) << 32;
+                ws.N |= ((quint64) data[6]) << 24;
+                ws.N |= ((quint64) data[7]) << 16;
+                ws.N |= ((quint64) data[8]) << 8;
+                ws.N |= ((quint64) data[9]) << 0;
+                i = 10;
+            }
+            if (ws.mask)
+            {
+                ws.masking_key[0] = ((quint8) data[i+0]) << 0;
+                ws.masking_key[1] = ((quint8) data[i+1]) << 0;
+                ws.masking_key[2] = ((quint8) data[i+2]) << 0;
+                ws.masking_key[3] = ((quint8) data[i+3]) << 0;
+            }
+            else
+            {
+                ws.masking_key[0] = 0;
+                ws.masking_key[1] = 0;
+                ws.masking_key[2] = 0;
+                ws.masking_key[3] = 0;
+            }
+            if (_input_data.size() < ws.header_size+ws.N) return; // Need: ws.header_size+ws.N - _input_data.size()
+
+            // We got a whole message, now do something with it:
+            if
+            (
+                ws.opcode == wsheader_type::TEXT_FRAME
+                ||
+                ws.opcode == wsheader_type::BINARY_FRAME
+                ||
+                ws.opcode == wsheader_type::CONTINUATION
+            )
+            {
+                if (ws.mask) for (size_t i = 0; i != ws.N; ++i) _input_data[i+ws.header_size] ^= ws.masking_key[i&0x3];
+                emit messageReceived(QString::fromUtf8(QByteArray((char*)_input_data.data()+ws.header_size, ws.N)));
+            }
+            else if (ws.opcode == wsheader_type::PING)
+            {
+                if (ws.mask) for (size_t i = 0; i != ws.N; ++i) _input_data[i+ws.header_size] ^= ws.masking_key[i&0x3];
+                sendData(wsheader_type::PONG, QByteArray((char*)_input_data.data()+ws.header_size, ws.N));
+            }
+            else if (ws.opcode == wsheader_type::PONG) ;
+            else if (ws.opcode == wsheader_type::CLOSE) close();
+            else
+            {
+                emit socketError("invalid websocket message", "websockets");
+                close();
+            }
+
+            _input_data.erase(_input_data.begin(), _input_data.begin() + ws.header_size+(size_t)ws.N);
+        }
+    }
 };
 
 class WebSocketClient : public QObject
@@ -279,12 +375,13 @@ public:
     Q_PROPERTY(QString extensions MEMBER _extensions)
     Q_PROPERTY(QString protocol MEMBER _protocol)
     Q_PROPERTY(bool mask MEMBER _mask)
+    Q_PROPERTY(bool ignoreSslErrors MEMBER _ignoreSslErrors)
     Q_PROPERTY(ReadyState state READ state NOTIFY stateChanged)
 
     Q_DISABLE_COPY(WebSocketClient)
 
 signals:
-    void toOpen(const QString& url, const QString& origin, const QString& extensions, const QString& protocol, bool mask);
+    void toOpen(const QString& url, const QString& origin, const QString& extensions, const QString& protocol, bool mask, bool ignoreSslErrors);
     void toPing();
     void toSend(const QString& message);
     void toClose();
@@ -321,7 +418,7 @@ public:
     }
 
 public slots:
-    void open() { emit toOpen(_url, _origin, _extensions, _protocol, _mask); }
+    void open() { emit toOpen(_url, _origin, _extensions, _protocol, _mask, _ignoreSslErrors); }
     void ping() { emit toPing(); }
     void send(const QString& text) { emit toSend(text); }
     void close() { emit toClose(); }
@@ -341,6 +438,7 @@ private:
     QString _extensions;
     QString _protocol;
     bool _mask = true;
+    bool _ignoreSslErrors = true;
     ReadyState _state = ReadyState::CLOSED;
 };
 
